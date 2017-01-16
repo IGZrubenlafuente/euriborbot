b9ba8e8b3e79517c487aa0e591f8197806c4ebfd
IGZrubenlafuente <ruben.lafuente@intelygenz.com>
2017-01-16 17:57:36 +0100

    extract the web request and data parse to a function

--- bin/app.js
+++ bin/app.js
@@ -8,33 +8,59 @@ const T = new Twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
-request.get(
-  {
-    method: 'GET',
-    uri: process.env.EURIBOR_SOURCE_URL
-  },
-  function (error, response, body) {
-    if (error) {
-      console.log('request error =', error)
-      return
-    }
-    // console.log('body =', body);
-    const $ = cheerio.load(body)
-    // console.log($('table.listado tr').eq(1))
-    const date = $('table.listado tr').eq(1).children('td').eq(0).text()
-    const value = $('table.listado tr').eq(1).children('td').eq(1).text()
-    // console.log(date, value);
-    const tweetMessage = '#Euribor ' + date + ': ' + value
-    // console.log('tweetMessage =', tweetMessage);
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    request.get(
+      {
+        method: 'GET',
+        uri: process.env.EURIBOR_SOURCE_URL
+      },
+      function (error, response, body) {
+        if (error) {
+          // console.log('request error =', error)
+          reject(error)
+        }
+        // console.log('body =', body);
+        const $ = cheerio.load(body)
+        // console.log($('table.listado tr').eq(1))
+        const trListado = $('table.listado tr')
+        if (trListado.length) {
+          // we skip the first TR (it contains the headers)
+          const firstValueRowTDs = trListado.eq(1).children('td')
+          const date = firstValueRowTDs.eq(0).text()
+          const value = firstValueRowTDs.eq(1).text()
+          const euriborData = {
+            date,
+            value
+          }
+          resolve(euriborData)
+        } else {
+          const error = 'no row found in body'
+          // console.log(error)
+          reject(error)
+        }
+      }
+    )
+  })
+}
+
+getData()
+  .then(euriborData => {
+    // console.log('euriborData =', euriborData)
+    const tweetMessage = '#Euribor ' + euriborData.date + ': ' + euriborData.value
+    console.log('tweetMessage =', tweetMessage)
     T.post('statuses/update',
       {status: tweetMessage},
-      function (error, data, response) {
+      function (error, tweetData, response) {
         if (error) {
           console.log('twit error =', error)
           return
         }
-        // console.log('post data =', data);
+        // console.log('post tweetData =', tweetData);
       }
     )
-  }
-)
+  })
+  .catch(err => {
+    console.log('err =', err)
+  })
+
